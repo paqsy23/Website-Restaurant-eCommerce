ce15c14fa5c245a80afc34dfb7fafbaf6c997d58
Stefan Ruben <stefanruben23@gmail.com>
2020-10-19 17:05:21 +0700

    Initial commit

---
+++ dbecommerce.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2020 at 05:01 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbecommerce`
+--
+CREATE DATABASE IF NOT EXISTS `dbecommerce` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dbecommerce`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alamat`
+--
+
+DROP TABLE IF EXISTS `alamat`;
+CREATE TABLE `alamat` (
+  `id_user` varchar(8) NOT NULL,
+  `kota` varchar(8) NOT NULL,
+  `jalan` varchar(255) NOT NULL,
+  `kodepos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+DROP TABLE IF EXISTS `barang`;
+CREATE TABLE `barang` (
+  `id_barang` varchar(8) NOT NULL,
+  `id_kategori` varchar(8) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `stock` int(10) NOT NULL,
+  `deskripsi` varchar(500) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `berat (kg)` int(10) NOT NULL,
+  `status` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE `cart` (
+  `id_barang` varchar(8) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `pesan` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+CREATE TABLE `chat` (
+  `id_chat` varchar(8) NOT NULL,
+  `id_chatroom` varchar(8) NOT NULL,
+  `pesan` varchar(500) NOT NULL,
+  `tanggal` date NOT NULL,
+  `status` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatroom`
+--
+
+DROP TABLE IF EXISTS `chatroom`;
+CREATE TABLE `chatroom` (
+  `id_chatroom` varchar(8) NOT NULL,
+  `id_user` varchar(8) NOT NULL,
+  `status` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dtrans`
+--
+
+DROP TABLE IF EXISTS `dtrans`;
+CREATE TABLE `dtrans` (
+  `id_dtrans` varchar(8) NOT NULL,
+  `id_htrans` varchar(8) NOT NULL,
+  `id_barang` varchar(8) NOT NULL,
+  `quantity` int(10) NOT NULL,
+  `subtotal` int(10) NOT NULL,
+  `pesan` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `htrans`
+--
+
+DROP TABLE IF EXISTS `htrans`;
+CREATE TABLE `htrans` (
+  `id_trans` varchar(8) NOT NULL,
+  `total` int(10) NOT NULL,
+  `status` varchar(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `h_dtrans`
+--
+
+DROP TABLE IF EXISTS `h_dtrans`;
+CREATE TABLE `h_dtrans` (
+  `id_htrans` varchar(8) NOT NULL,
+  `id_trans` varchar(8) NOT NULL,
+  `id_user_pembeli` varchar(8) NOT NULL,
+  `nama_ekspedisi` varchar(255) NOT NULL,
+  `harga_ekspedisi` int(10) NOT NULL,
+  `tanggal` date NOT NULL,
+  `total` int(10) NOT NULL,
+  `status` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+DROP TABLE IF EXISTS `kategori`;
+CREATE TABLE `kategori` (
+  `id_kategori` varchar(8) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+DROP TABLE IF EXISTS `kota`;
+CREATE TABLE `kota` (
+  `id_kota` varchar(8) NOT NULL,
+  `id_provinsi` varchar(8) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `provinsi`
+--
+
+DROP TABLE IF EXISTS `provinsi`;
+CREATE TABLE `provinsi` (
+  `id_provinsi` varchar(8) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review` (
+  `id_review` varchar(8) NOT NULL,
+  `id_barang` varchar(8) NOT NULL,
+  `id_dtrans` varchar(8) NOT NULL,
+  `rating` int(5) NOT NULL,
+  `pesan` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id_user` varchar(8) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `notelp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alamat`
+--
+ALTER TABLE `alamat`
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `kota` (`kota`);
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `chat`
+--
+ALTER TABLE `chat`
+  ADD PRIMARY KEY (`id_chat`),
+  ADD KEY `id_chatroom` (`id_chatroom`);
+
+--
+-- Indexes for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  ADD PRIMARY KEY (`id_chatroom`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `dtrans`
+--
+ALTER TABLE `dtrans`
+  ADD PRIMARY KEY (`id_dtrans`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_htrans` (`id_htrans`);
+
+--
+-- Indexes for table `htrans`
+--
+ALTER TABLE `htrans`
+  ADD PRIMARY KEY (`id_trans`);
+
+--
+-- Indexes for table `h_dtrans`
+--
+ALTER TABLE `h_dtrans`
+  ADD PRIMARY KEY (`id_htrans`),
+  ADD KEY `id_user_pembeli` (`id_user_pembeli`),
+  ADD KEY `id_trans` (`id_trans`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id_kota`),
+  ADD KEY `id_provinsi` (`id_provinsi`);
+
+--
+-- Indexes for table `provinsi`
+--
+ALTER TABLE `provinsi`
+  ADD PRIMARY KEY (`id_provinsi`);
+
+--
+-- Indexes for table `review`
+--
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id_review`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_dtrans` (`id_dtrans`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alamat`
+--
+ALTER TABLE `alamat`
+  ADD CONSTRAINT `alamat_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `alamat_ibfk_2` FOREIGN KEY (`kota`) REFERENCES `kota` (`id_kota`);
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
+-- Constraints for table `chat`
+--
+ALTER TABLE `chat`
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`id_chatroom`) REFERENCES `chatroom` (`id_chatroom`);
+
+--
+-- Constraints for table `chatroom`
+--
+ALTER TABLE `chatroom`
+  ADD CONSTRAINT `chatroom_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `dtrans`
+--
+ALTER TABLE `dtrans`
+  ADD CONSTRAINT `dtrans_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `dtrans_ibfk_2` FOREIGN KEY (`id_htrans`) REFERENCES `htrans` (`id_trans`);
+
+--
+-- Constraints for table `h_dtrans`
+--
+ALTER TABLE `h_dtrans`
+  ADD CONSTRAINT `h_dtrans_ibfk_1` FOREIGN KEY (`id_user_pembeli`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `h_dtrans_ibfk_2` FOREIGN KEY (`id_trans`) REFERENCES `htrans` (`id_trans`);
+
+--
+-- Constraints for table `kota`
+--
+ALTER TABLE `kota`
+  ADD CONSTRAINT `kota_ibfk_1` FOREIGN KEY (`id_provinsi`) REFERENCES `provinsi` (`id_provinsi`);
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`),
+  ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`id_dtrans`) REFERENCES `dtrans` (`id_dtrans`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
